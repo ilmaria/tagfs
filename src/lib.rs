a1a70d7bbff1882ca3b9c6c3cf1904fca73395ca
Ilmari Autio <ilmari.autio@gmail.com>
2017-12-09 00:45:35 +0200

    Add skeleton structure to ipfs api

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
+extern crate futures;
+extern crate hyper;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate tokio_core;
+
+mod ipfs;
+
 #[cfg(test)]
 mod tests {
+    use super::ipfs;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let s = "hei";
+        let a = ipfs::add(&s);
+        assert_eq!(a + 1, 4);
     }
 }
